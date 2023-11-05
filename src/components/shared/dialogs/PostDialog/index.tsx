5a4a7d6303d96d43b5d97a3963935354d67cfee3
ostrich-t <t.nakajima.busi@gmail.com>
2023-11-05 12:40:00 +0900

    submit when press enter key

--- src/components/shared/dialogs/PostDialog/index.tsx
+++ src/components/shared/dialogs/PostDialog/index.tsx
@@ -22,10 +22,18 @@ export const PostDialog: FC = () => {
 
   const handleClose = usePostStore((state) => state.closeCreateDialog);
   const [content, setContent] = React.useState("");
+  const trimmedContent = content.trim();
 
   const handlePost = () => {
     createPostMutation.mutate({ content: content });
     handleClose();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (trimmedContent && e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handlePost();
+    }
   };
 
   useEffect(() => {
@@ -39,12 +47,13 @@ export const PostDialog: FC = () => {
         className="textarea textarea-bordered h-32 w-full"
         placeholder="Wright something..."
         onChange={(e) => setContent(e.target.value)}
+        onKeyDown={handleKeyDown}
       ></textarea>
       <div className="content-right mt-2">
         <button
           className="btn btn-primary ml-auto"
           onClick={handlePost}
-          disabled={!content}
+          disabled={!trimmedContent}
         >
           Post
         </button>
